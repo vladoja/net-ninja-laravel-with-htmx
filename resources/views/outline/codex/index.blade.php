143cf88f798a9bbc8ebe0b4d6d697bd78ad6c653
vladoja <dr.jalo2@gmail.com>
2026-02-10 23:19:38 +0100

    Alpine L2 Filter element with button. Btn styling. Alpine set active class.

--- resources/views/outline/codex/index.blade.php
+++ resources/views/outline/codex/index.blade.php
@@ -4,7 +4,7 @@
     <h1 class="page-title">Codex</h1>
 
     @fragment('codex-entry-list')
-        <div class="codex-list content" id="codex-list">
+        <div class="codex-list content" id="codex-list" x-data="{ filter: 'none' }">
             <div class="flex justify-between items-center mb-8">
                 <h2 class="text-xl ml-2 font-bold">Codex Entries</h2>
                 <a href="{{ route('outline.codex.create') }}" class="btn inline-block"
@@ -13,6 +13,15 @@
                     Add a New Codex Entry
                 </a>
             </div>
+
+            @if ($isHtmx)
+                <div class="filters mb-4">
+                    <p>Filters:</p>
+                    <button class="btn btn-tertiary" x-on:click="filter != 'all'? filter = 'all' : filter = 'none'"
+                        :class="{ 'active': filter === 'all' }">
+                        All</button>
+                </div>
+            @endif
 
             @php
                 $types = ['character', 'item', 'location'];
